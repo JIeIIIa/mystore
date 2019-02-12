efc4a095f9120c762d93811d9b197673a738cc3c
JIeIIIa <stealth@bigmir.net>
2019-02-12 11:19:05 +0200

    Add ProductServiceFactory to create instance of ProductService. Fix typo in log4j2

--- src/main/java/com/onishchenko/oleksii/mystore/service/ProductServiceFactory.java
+++ src/main/java/com/onishchenko/oleksii/mystore/service/ProductServiceFactory.java
@@ -3,6 +3,10 @@ package com.onishchenko.oleksii.mystore.service;
 
 import com.onishchenko.oleksii.mystore.dao.ProductDaoFactory;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
 /**
  * This class provides a factory for creating an Product Service instance.
  */
@@ -15,5 +19,27 @@ public class ProductServiceFactory {
      */
     public static ProductService getInstance(String directoryPathToSaveOrders) {
         return new ProductServiceImpl(ProductDaoFactory.singleton(), directoryPathToSaveOrders);
+    }
+
+    /**
+     * Returns path to 'data' directory in the Tomcat directory or default path
+     *
+     * @return path to 'data' directory in the Tomcat directory if the 'catalina.home'
+     * system property is set; <code>Paths.get("/data")</code> otherwise
+     * */
+    public static Path getDataPath() {
+        Path tomcatPath = Paths.get(
+                Optional.ofNullable(System.getProperty("catalina.home")).orElse("/")
+        );
+        return tomcatPath.resolve("data");
+    }
+
+    /**
+     * Returns a {@link ProductServiceImpl} instance with default settings
+     *
+     * @return a {@link ProductServiceImpl} instance with default settings
+     */
+    public static ProductService getInstance() {
+        return new ProductServiceImpl(ProductDaoFactory.singleton(), getDataPath().toString());
     }
 }
--- src/test/java/com/onishchenko/oleksii/mystore/service/ProductServiceFactoryTest.java
+++ src/test/java/com/onishchenko/oleksii/mystore/service/ProductServiceFactoryTest.java
@@ -2,18 +2,54 @@ package com.onishchenko.oleksii.mystore.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductServiceFactoryTest {
     @Test
     void getInstance() {
-        //Given
-
         //When
         ProductService productService = ProductServiceFactory.getInstance("path");
 
         //Then
         assertThat(productService).isNotNull()
                 .isInstanceOf(ProductServiceImpl.class);
+    }
+
+    @Test
+    void defaultGetInstance() {
+        //When
+        ProductService productService = ProductServiceFactory.getInstance();
+
+        //Then
+        assertThat(productService).isNotNull()
+                .isInstanceOf(ProductServiceImpl.class);
+    }
+
+    @Test
+    void defaultDataPath() {
+        //Given
+        Path expected = Paths.get("/data");
+
+        //When
+        Path dataPath = ProductServiceFactory.getDataPath();
+
+        //Then
+        assertThat(dataPath).isEqualTo(expected);
+    }
+
+    @Test
+    void dataPath() {
+        //Given
+        System.setProperty("catalina.home", "/somePath");
+        String expected = "/somePath/data";
+
+        //When
+        Path dataPath = ProductServiceFactory.getDataPath();
+
+        //Then
+        assertThat(dataPath).isEqualTo(Paths.get(expected));
     }
 }
